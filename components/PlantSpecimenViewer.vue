a94b985c0994095aebc0db6232bd5625a931d833
Ron Snyder <Ronald.Snyder@ithaka.org>
2020-09-20 17:28:04 -0400

    Updated specimen viewer to use OSD

--- components/PlantSpecimenViewer.vue
+++ components/PlantSpecimenViewer.vue
@@ -19,7 +19,6 @@
                 :items="specimens.specimens"
                 :width="width"
                 :height="height - 46"
-                initial-mode="static"
                 default-fit="cover"
               >
               </image-viewer>
@@ -33,14 +32,13 @@
 
 <script>
 
-const dependencies = [
-];
+const dependencies = []
 
 module.exports = {
     name: "PlantSpecimenViewer",
     props: { items: Array, width: Number, height: Number },
     components: {
-        imageViewer: 'url:https://raw.githubusercontent.com/jstor-labs/ve-components/master/components/ImageViewer/index.vue'
+        imageViewer: 'url:/components/OpenSeadragonViewer.vue'
     },
     data: () => ({
         tab: undefined,
@@ -52,14 +50,14 @@ module.exports = {
                 width: `${this.width}px`,
                 height: `${this.height}px`,
                 padding: 0,
-            };
+            }
         },
         innerContainerStyle() {
             return {
                 height: `${this.height - 48}px`,
                 padding: 0,
                 overflowY: "auto !important",
-            };
+            }
         },
     },
     mounted() {
@@ -67,50 +65,41 @@ module.exports = {
     },
     methods: {
         init() {
-            console.log(`${this.$options.name}.mounted: height=${this.height} width=${this.width}`, this.mapDef)
+            console.log(`${this.$options.name}.mounted: height=${this.height} width=${this.width}`)
         },
         getSpecimenMetadata(item) {
-            const id = item.jpid || item.eid;
+            const id = item.jpid || item.eid
             if (item.specimensMetadata) {
-                this.specimensByTaxon = [
-                    ...this.specimensByTaxon,
-                    item.specimensMetadata,
-                ];
+                this.specimensByTaxon = [...this.specimensByTaxon, item.specimensMetadata]
             } else {
                 const args = Object.keys(item)
                 .filter((arg) => ["max", "reverse"].includes(arg))
-                .map((arg) => `${arg}=${item[arg]}`);
-                const url = `https://plant-humanities.app/specimens/${id}` + (args ? `?${args.join("&")}` : "");
+                .map((arg) => `${arg}=${item[arg]}`)
+                const url = `/specimens/${id}` + (args ? `?${args.join("&")}` : "")
+                console.log(url)
                 fetch(url).then((resp) => resp.json())
-                    .then((specimensMetadata) => {
-                        if (specimensMetadata.specimens.length > 0) {
-                            specimensMetadata.caption = item.label;
-                            specimensMetadata.specimens.forEach((specimen) => {
-                                const defaultImage = specimen.images.find((img) => img.type === "default");
-                                specimen.url = defaultImage.url;
-                                specimen.title = specimen.description;
-                            });
-                            this.specimensByTaxon = [
-                                ...this.specimensByTaxon,
-                                specimensMetadata
-                            ];
-                            //this.$store.dispatch("updateItem", {
-                            //    ...item,
-                            //    ...{ specimensMetadata }
-                            //});
-                        }
-                    });
+                .then((specimensMetadata) => {
+                    if (specimensMetadata.specimens.length > 0) {
+                        specimensMetadata.caption = item.label || item.title
+                        specimensMetadata.specimens.forEach((specimen) => {
+                            specimen.url = specimen.images.find((img) => img.type === 'best').url
+                            specimen.title = specimen.description
+                        })
+                        this.specimensByTaxon = [...this.specimensByTaxon, specimensMetadata]
+                        console.log('specimensByTaxon', this.specimensByTaxon)
+                    }
+                })
             }
-        },
+        }
     },
     watch: {
         items: {
             handler: function(current, prior) {
-                const currentItemIds = new Set(current.map((item) => item.id));
-                const priorItemIds = new Set((prior || []).map((item) => item.id));
+                const currentItemIds = new Set(current.map((item) => item.id))
+                const priorItemIds = new Set((prior || []).map((item) => item.id))
                 if (!this.eqSet(currentItemIds, priorItemIds)) {
-                    this.specimensByTaxon = [];
-                    this.items.forEach((item) => this.getSpecimenMetadata(item));
+                    this.specimensByTaxon = []
+                    this.items.forEach((item) => this.getSpecimenMetadata(item))
                 }
             },
             immediate: true
